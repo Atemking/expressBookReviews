8e588dd6fc1c770e925a2ff7b68b56e6a4ff8b93
Atemking <atemafackingsley7@gmail.com>
2023-11-27 10:18:23 -0500

    update on my project

---
+++ router/auth_users.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
+let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+   if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", function (req, res) {
+    const isbn = req.params.isbn;
+    let book = books[isbn]
+    if (book) { //Check is friend exists
+        let review = req.body.reviews;
+        //Add similarly for firstName
+        //Add similarly for lastName
+
+        //if DOB the DOB has been changed, update the DOB 
+        if(review) {
+            book["reviews"] = review
+        }
+        //Add similarly for firstName
+        //Add similarly for lastName
+        books[isbn]=books;
+        res.send(`Book with the email  ${isbn} updated.`);
+    }
+    else{
+        res.send("Unable to find book!");
+    }
+  });
+
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    if (isbn){
+        delete books[isbn]
+    }
+    res.send(`Book with the email  ${isbn} deleted.`);
+  });
+
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
